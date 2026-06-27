cf5f0ae8d573e0cb0f6310d408c21c897e9c13b9
Dmitry Zakharov <dzakarov@gmail.com>
2026-06-27 17:15:55 +0100

    main class restore
    Signed-off-by: Dmitry Zakharov <dzakarov@gmail.com>

--- src/main/java/sco3/McpServerApplication.java
+++ src/main/java/sco3/McpServerApplication.java
@@ -3,8 +3,7 @@ package sco3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "sco3")
-
+@SpringBootApplication
 public class McpServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(McpServerApplication.class, args);
